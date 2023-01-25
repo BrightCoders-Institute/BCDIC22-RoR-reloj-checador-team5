9b82699f4f9443fc358994a069d419c9eb5b1d00
oscaregr <bavuey@hotmail.com>
2023-01-25 16:06:23 -0600

    tabs chenge?

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,15 +1,15 @@
 class LoginController < ApplicationController
-	def index	
-	end
+  def index	
+  end
 
-	def create
-		admin_params.each do |key, value|
-			redirect_to admin_index_path if value == "admin"
-		end
-	end
+  def create
+    admin_params.each do |key, value|
+      redirect_to admin_index_path if value == "admin"
+    end
+  end
 
-	private
-		def admin_params
-			params.permit(:password)
-		end
+  private
+    def admin_params
+      params.permit(:password)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
 
-	# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-	root 'checks#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'checks#index'
 
-	resources :login, only: [:index, :create]
-	
-	resources :admin, only: [:index]
+  resources :login, only: [:index, :create]
+  
+  resources :admin, only: [:index]
 
-	resources :employees
-	resources :checks
-	# Defines the root path route ("/")
-	# root "articles#index"
+  resources :employees
+  resources :checks
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
