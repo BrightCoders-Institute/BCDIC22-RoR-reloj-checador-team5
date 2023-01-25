313e960d07f236d1735aa81b19a32ce614a23948
oscaregr <bavuey@hotmail.com>
2023-01-24 18:41:16 -0600

    Login Admin
    in this commit we made the implementation of the login and the design for the admin
    
    Co-authored-by: eduard montaño <Ewar22@users.noreply.github.com>
    Co-authored-by: Severo Iglesias <SEVERO96@users.noreply.github.com>
    Co-authored-by: Manuel Rios <maanuu.009@gmail.com>

---
+++ app/controllers/login_controller.rb
@@ -0,0 +1,15 @@
+class LoginController < ApplicationController
+  def index	
+  end
+
+	def create
+		admin_params.each do |key, value|
+			redirect_to admin_index_path if value == "admin"
+		end
+	end
+
+	private
+		def admin_params
+			params.permit(:password)
+		end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'checks#index'
+
+	resources :login, only: [:index, :create]
+	
+	resources :admin, only: [:index]
 
   resources :employees
   resources :checks
