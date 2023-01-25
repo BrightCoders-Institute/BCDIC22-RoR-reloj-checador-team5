ecc3fa3a7aaca3d20f327f2912d166c88616dd3e
eduard issael montaño arreola <117693797+Ewar22@users.noreply.github.com>
2023-01-24 19:15:15 -0600

    Desig Admin
    Design of the view of the admin
    
    Co-authored-by: xManuel007 <maanuu.009@gmail.com>
    Co-authored-by: oscaregr <oegr.el.may@gmail.com>
    Co-authored-by: Severo Iglesias <siglesias@ucol.mx>

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'checks#index'
 
   resources :employees
   resources :checks
+  resources :admin, only: [:index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
