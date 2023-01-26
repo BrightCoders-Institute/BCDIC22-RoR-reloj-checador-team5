677496da2ea4a600748cb2478e1432143c70355b
eduard issael montaño arreola <117693797+Ewar22@users.noreply.github.com>
2023-01-25 19:32:28 -0600

    Crud Completa
    Crud finished of the companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -11,7 +11,7 @@ class CompaniesController < ApplicationController
     @company = Company.new(company_params)
     
     if @company.save
-      redirect_to admin_index_path
+      redirect_to companies_path
     else
       render :new, status: :unprocessable_entity
     end
@@ -20,10 +20,25 @@ class CompaniesController < ApplicationController
   def edit
     company
   end
-  
+
+  def update
+    if company.update(company_params)
+      redirect_to companies_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    company.destroy
+    redirect_to companies_path
+  end
 
   private
     def company_params
       params.require(:company).permit(:name, :address)
+    end
+    def company
+      @company = Company.find(params[:id])
     end
 end
