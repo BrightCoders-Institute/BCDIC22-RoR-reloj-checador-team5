79368d995f604d561f0556d61ea4c065c65cccd1
Severo Iglesias <siglesias@ucol.mx>
2023-02-01 13:52:12 -0600

    environment configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-	config.hosts << 'https://reloj-checador-team5.up.railway.app'
+	config.hosts << 'relojchecadorfive.up.railway.app'
   # Code is not reloaded between requests.
   config.cache_classes = true
 
