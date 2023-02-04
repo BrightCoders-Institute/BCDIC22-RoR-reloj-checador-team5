bba0c052e7d0f2489628acda9c6d6f3e77f9a830
oscaregr <bavuey@hotmail.com>
2023-02-03 19:01:18 -0600

    delete user model/controller

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_secure_password
-
-  validates :username, presence: true, uniqueness: true
-  validates :password, length: {minimum: 5}, presence: true
-  
-end
